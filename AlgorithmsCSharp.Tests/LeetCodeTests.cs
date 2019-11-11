4e0bbd9cf280c86d73502a789107a0f4ca7c764d
Tony <mcconvilletony@gmail.com>
2019-11-10 18:47:07 -0800

    Added IsRectangleOverlap

---
+++ AlgorithmsCSharp.Tests/LeetCodeTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmsCSharp.Tests
+{
+    class LeetCodeTests
+    {
+
+        [Test]
+        public void RectangleOverlap_ValidInput_True()
+        {
+            int[] rec1 = { 0, 0, 2, 2 };
+            int[] rec2 = { 1, 1, 3, 3 };
+            Assert.IsTrue(LeetCode.IsRectangleOverlap(rec1, rec2));
+        }
+
+        [Test]
+        public void RectangleOverlap_ValidInput_False()
+        {
+            int[] rec1 = { 0, 0, 1, 1 };
+            int[] rec2 = { 1, 0, 2, 1 };
+            Assert.IsFalse(LeetCode.IsRectangleOverlap(rec1, rec2));
+        }
+    }
+}
